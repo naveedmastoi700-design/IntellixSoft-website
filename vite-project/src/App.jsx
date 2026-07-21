b88c91fc5c275fbe5ea3e8e49261c7ae195bea21
Muhammad Naveed <naveedmastoi700@gmail.com>
2026-07-21 23:30:12 +0500

    Add new course routes for DataAutomation, Shopify, Youtube, SEO, and Computer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,9 +8,11 @@ import OurTeam from "./Pages/Ourteam";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Courses from "./Pages/Courses";
+import Youtube from "./Pages/Youtube";
 import Client from "./Components/Client";
+import Shopify from "./Pages/Shopify";
 import Footer from "./Components/Footer";
-
+import DataAutomation from "./Pages/Data-Automation";
 import WebAppDevelopment from "./Pages/WebAppDevelopment";
 import MobileAppDevelopment from "./Pages/MobileAppDevelopment";
 import PythonMachineLearning from "./Pages/PythonMachineLearning";
@@ -19,7 +21,8 @@ import DigitalMarketing from "./Pages/DigitalMarketing";
 import BusinessDevelopment from "./Pages/BusinessDevelopment";
 import BackendWebDevelopmentwithPython from "./Pages/BackendWebDevelopmentwithPython";
 import DataScienceAndAutomationWithPython from "./Pages/DataScienceAndAutomationWithPython";
-
+import SEO from "./Pages/SEO";
+import Computer from "./Pages/Computer";
 import "./App.css";
 import { FaRegMoneyBillAlt } from "react-icons/fa";
 
@@ -39,6 +42,26 @@ function App() {
         <Route path="/WebAppDevelopment" element={<WebAppDevelopment />} />
         <Route path="/MobileAppDevelopment" element={<MobileAppDevelopment />} />
         <Route path="/PythonMachineLearning" element={<PythonMachineLearning />} />
+        <Route
+          path="/courses/data-automation"
+          element={<DataAutomation />}
+        />
+        <Route
+          path="/courses/shopify"
+          element={<Shopify />}
+        />
+        <Route
+          path="/courses/Youtube"
+          element={<Youtube />}
+        />
+        <Route
+          path="/courses/seo"
+          element={<SEO />}
+        />
+        <Route
+          path="/courses/Computer"
+          element={<Computer />}
+        />
         <Route path="/GraphicDesigner" element={<GraphicDesigner />} />
         <Route path="/DigitalMarketing" element={<DigitalMarketing />} />
         <Route path="/BusinessDevelopment" element={<BusinessDevelopment />} />
@@ -48,6 +71,7 @@ function App() {
         <Route path="/courses/web-app-development" element={<WebAppDevelopment />} />
         <Route path="/courses/mobile-app-development" element={<MobileAppDevelopment />} />
         <Route path="/courses/python-machine-learning" element={<PythonMachineLearning />} />
+
         <Route path="/courses/graphic-designer" element={<GraphicDesigner />} />
         <Route path="/courses/digital-marketing" element={<DigitalMarketing />} />
         <Route path="/courses/business-development" element={<BusinessDevelopment />} />
